e8cd37cd3325fcd0989691e950c571e3b963b0b4
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 02:31:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,6 +9,7 @@ end
 def frist_element (arr)
  #return arr[0]
  return.first
+ # this is the most difficult problem because I even tried the slice method and that doesn't work 
 end
 #
 def third_element (arr)
